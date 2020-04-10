4767703bde4f57804850662283e5e0a2c4fc0187
RaphaelPLM <raphaelplmuller@icloud.com>
2020-04-10 13:32:48 -0300

    Setup knex and Procfile for production

--- knexfile.js
+++ knexfile.js
@@ -9,45 +9,16 @@ module.exports = {
 			user: process.env.DEVELOPMENT_DATABASE_USER,
 			password: process.env.DEVELOPMENT_DATABASE_PASSWORD
 		},
-		pool: {
-			min: 2,
-			max: 10
-		},
 		migrations: {
 			directory: './src/database/migrations'
 		},
 		useNullAsDefault: true
 	},
-
-	staging: {
-		client: 'postgresql',
-		connection: {
-			database: 'my_db',
-			user: 'username',
-			password: 'password'
-		},
-		pool: {
-			min: 2,
-			max: 10
-		},
-		migrations: {
-			tableName: 'knex_migrations'
-		}
-	},
-
 	production: {
 		client: 'postgresql',
-		connection: {
-			database: 'my_db',
-			user: 'username',
-			password: 'password'
-		},
-		pool: {
-			min: 2,
-			max: 10
-		},
+		connection: process.env.DATABASE_URL,
 		migrations: {
-			tableName: 'knex_migrations'
+			directory: './src/database/migrations'
 		}
 	}
 };
--- src/index.js
+++ src/index.js
@@ -4,8 +4,10 @@ const cors = require('cors');
 
 const app = express();
 
+const port = process.env.PORT || 8000;
+
 app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(8000);
+app.listen(port, () => console.log('Listening on port ', port));
