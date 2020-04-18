04b286542f7f43f360551a38d80ad0835794a5b7
Raphael Muller <raphaelplmuller@icloud.com>
2020-04-18 17:30:27 -0300

    Add create_cards migration

---
+++ src/database/migrations/20200418171624_create_cards.js
@@ -0,0 +1,16 @@
+exports.up = function (knex) {
+  return knex.schema.createTable("cards", function (table) {
+    table.increments();
+
+    table.string("url").unique();
+    table.string("status").notNullable().defaultTo("Pending");
+    table.integer("member_id").notNullable();
+    table.timestamp("created_at").notNullable().defaultTo(knex.raw("now()"));
+
+    table.foreign("member_id").references("id").inTable("members");
+  });
+};
+
+exports.down = function (knex) {
+  return knex.schema.dropTable("cards");
+};
