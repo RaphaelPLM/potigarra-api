6113136b03a2a915589418b58ce71b5b4de0f050
Raphael Müller <raphaelplmuller@icloud.com>
2020-04-21 00:59:47 -0300

    Add member info to cards index return

--- src/controllers/card_controller.js
+++ src/controllers/card_controller.js
@@ -2,12 +2,19 @@ const connection = require("../database/connection");
 
 module.exports = {
   async index(request, response) {
-    const cards = await connection("cards").select(
-      "id",
-      "url",
-      "status",
-      "member_id"
-    );
+    const cards = await connection("cards")
+      .join("members", "members.id", "=", "cards.member_id")
+      .select(
+        "*",
+        "members.username",
+        "members.cpf",
+        "members.rg",
+        "members.phone_number",
+        "members.class_number",
+        "members.gender",
+        "members.birthdate",
+        "members.created_at"
+      );
 
     const [count] = await connection("cards").count();
 
